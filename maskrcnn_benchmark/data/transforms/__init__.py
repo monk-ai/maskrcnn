75d1e3ce45d859b8ae482334d9a91b5449357a8f
Ruben Scherman <ruben.scherman@gmail.com>
2019-09-23 18:49:33 +0200

    modified data augmentation

--- maskrcnn_benchmark/data/transforms/__init__.py
+++ maskrcnn_benchmark/data/transforms/__init__.py
@@ -1,8 +1,16 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
-from .transforms import Compose
-from .transforms import Resize
-from .transforms import RandomHorizontalFlip
-from .transforms import ToTensor
-from .transforms import Normalize
+# from .transforms import Compose
+# from .transforms import Resize
+# from .transforms import RandomHorizontalFlip
+# from .transforms import ToTensor
+# from .transforms import Normalize
+#
+# from .build import build_transforms
 
-from .build import build_transforms
+
+from .transforms_v2 import Compose
+from .transforms_v2 import Resize
+from .transforms_v2 import ToTensor
+from .transforms_v2 import Normalize
+
+from .build_v2 import build_transforms
