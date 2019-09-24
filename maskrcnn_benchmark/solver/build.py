fd50083e3bbb42b24082b9f060fa70c09bcc932a
Ruben Scherman <ruben.scherman@gmail.com>
2019-09-24 19:01:35 +0200

    added paths

--- maskrcnn_benchmark/solver/build.py
+++ maskrcnn_benchmark/solver/build.py
@@ -16,7 +16,7 @@ def make_optimizer(cfg, model):
             weight_decay = cfg.SOLVER.WEIGHT_DECAY_BIAS
         params += [{"params": [value], "lr": lr, "weight_decay": weight_decay}]
 
-    if cfg.SOlVER.TYPE.lower() == "sgd":
+    if cfg.SOLVER.TYPE.lower() == "sgd":
         optimizer = torch.optim.SGD(params, lr, momentum=cfg.SOLVER.MOMENTUM)
     elif cfg.SOLVER.TYPE.lower() == "adam":
         optimizer = torch.optim.Adam(params, lr, betas=(0.9, 0.999))
