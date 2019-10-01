f52b4d2d8a940aad2a1bf2e2763f98704752a72d
Ruben Scherman <ruben.scherman@gmail.com>
2019-10-01 18:57:39 +0200

    changed adam to adamw weight decay

--- maskrcnn_benchmark/solver/build.py
+++ maskrcnn_benchmark/solver/build.py
@@ -19,7 +19,7 @@ def make_optimizer(cfg, model):
     if cfg.SOLVER.TYPE.lower() == "sgd":
         optimizer = torch.optim.SGD(params, lr, momentum=cfg.SOLVER.MOMENTUM)
     elif cfg.SOLVER.TYPE.lower() == "adam":
-        optimizer = torch.optim.Adam(params, lr, betas=(0.9, 0.999))
+        optimizer = torch.optim.AdamW(params, lr, betas=(0.9, 0.999))
     else:
         import sys
         sys.exit(1)
